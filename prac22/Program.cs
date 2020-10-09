d5b1b1733db5a5cae4ac0518525fc91d798c5ca6
Rein-xmmm <72437331+Rein-xmmm@users.noreply.github.com>
2020-10-09 23:03:36 +0900

    Initial commit

---
+++ prac22/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pain User = new pain();
+            Console.WriteLine("Ваши текущие параметры:");
+            Console.WriteLine();
+            Console.WriteLine($"Имя - {User.name}");
+            Console.WriteLine($"Фамилия - {User.firstname}");
+            Console.WriteLine($"Возраст - {User.years}");
+            Console.WriteLine($"Рост (См) - {User.height}");
+            Console.WriteLine($"Вес (Кг) - {User.weight}");
+           
+
+            Console.WriteLine();//Имя
+            pain.SetName();
+            Console.WriteLine();//Фамилия
+            pain.Setfirstname();
+            Console.WriteLine();//Лет
+            pain.Setyears();
+            Console.WriteLine();//Рост
+            pain.Setheight();
+            Console.WriteLine();//Вес
+            pain.Setweight();
+            
+
+
+        }
+    }
+}
---
+++ prac22/pain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac22
+{
+    class pain
+
+    {
+        public string name = "Алишер";
+        public string firstname = "Бозорбаев";
+        public int weight = 50;
+        public int height = 164;
+        public int years = 17;
+
+        static internal string SetName()
+        {
+            Console.WriteLine("Введите ваше имя");
+            string d = Console.ReadLine();
+            Console.WriteLine($"Ваше новое имя - {d} (старой было лучше)");
+            return d;
+        }
+        static internal string Setfirstname()
+        {
+            Console.WriteLine("Введите вашу фамилию");
+            string o = Console.ReadLine();
+            Console.WriteLine($"Ваша новая фамилия - {o}");
+            return o;
+        }
+
+        static internal int Setweight()
+        {
+            Console.WriteLine("Введите ваш вес (Кг)");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваш новый вес (Кг) - {A}");
+            return A;
+        }
+        static internal int Setheight()
+        {
+            Console.WriteLine("Введите ваш рост (См)");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваш новый рост (См) - {a}");
+            return a;
+        }
+
+        static internal int Setyears()
+        {
+            Console.WriteLine("Введите ваш возраст");
+            int godik = Convert.ToInt32(Console.ReadLine());
+            {
+                if (godik >= 20)
+
+                {
+                    Console.WriteLine($"вам уже {godik} пара уже на пенсию? :D ");
+                    Console.ReadLine();
+                }
+
+            }
+            return godik;
+        }
+    }
+}
